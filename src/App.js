f7d04208d3364a6f9cf85d1b6946b0846d490ebb
supakorn <spkdosomething@gmail.com>
2023-04-07 16:49:39 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import FormComponent from './Component/FormComponent';
+
+function App() {
+  return (
+    <div className="App">
+        <FormComponent/>
+    </div>
+  );
+}
+
+export default App;
